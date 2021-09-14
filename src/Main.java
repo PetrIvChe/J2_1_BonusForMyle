4bea81c6fd797e322f785a33396c9df174b47c59
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-14 12:44:27 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketCost = 10321 ;
+        int rublesPerMyle = 20;
+        int  bonusRubles =ticketCost/rublesPerMyle;
+        System.out.println("Вам начислено "+ bonusRubles + " бонусных рублей.");    }
+}
