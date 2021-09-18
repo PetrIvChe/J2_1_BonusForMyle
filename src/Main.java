d11624c8ef05f01a4b91a4e64bfacdafab5b9516
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-18 14:56:00 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int ticketCost = 10321 ;
+        int ticketCost = 10321;
         int rublesPerMyle = 20;
-        int  bonusRubles =ticketCost/rublesPerMyle;
-        System.out.println("Вам начислено "+ bonusRubles + " бонусных рублей.");    }
+        int bonusRubles = ticketCost / rublesPerMyle;
+        System.out.println("Вам начислено " + bonusRubles + " бонусных рублей.");
+    }
 }
